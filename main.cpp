e58438cadd4e09f9e8fb0abed81671adacf0adad
Danya <danilomaster2003@ukr.net>
2022-01-23 17:57:12 +0200

    Translated GUI

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.setWindowTitle("Лабораторна робота №4, Деркач Д.А. ПІ-201");
+    w.setWindowTitle("Spanning-tree");
     w.show();
     return a.exec();
 }
